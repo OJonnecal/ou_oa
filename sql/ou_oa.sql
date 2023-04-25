73837337704afd47e55e6a05d611b519a011e27d
OuJiajun <95907237+OJonnecal@users.noreply.github.com>
2023-04-26 00:23:09 +0800

    更新sql文件

--- sql/ou_oa.sql
+++ sql/ou_oa.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : MySQL
+ Source Server         : MySQL80
  Source Server Type    : MySQL
  Source Server Version : 80028
  Source Host           : localhost:3306
@@ -11,29 +11,11 @@
  Target Server Version : 80028
  File Encoding         : 65001
 
- Date: 24/04/2023 17:23:23
+ Date: 26/04/2023 00:22:18
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for address_list
--- ----------------------------
-DROP TABLE IF EXISTS `address_list`;
-CREATE TABLE `address_list`  (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NULL DEFAULT NULL COMMENT '用户id',
-  `contacts_id` int NULL DEFAULT NULL COMMENT '联系人id',
-  `is_deleted` tinyint NULL DEFAULT 0,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `user_id`(`user_id`) USING BTREE,
-  CONSTRAINT `address_list_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '通讯录表' ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of address_list
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for assignment
@@ -50,7 +32,7 @@ CREATE TABLE `assignment`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `user_id`(`user_id`) USING BTREE,
   CONSTRAINT `assignment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '任务清单表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '任务清单表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of assignment
@@ -58,7 +40,7 @@ CREATE TABLE `assignment`  (
 INSERT INTO `assignment` VALUES (1, NULL, '1', '1', '2023-04-24 05:04:44', 0, 1);
 INSERT INTO `assignment` VALUES (2, NULL, '1', '1', '2023-04-24 05:07:41', 1, 1);
 INSERT INTO `assignment` VALUES (3, NULL, '1', '1', '2023-04-24 05:11:44', 1, 1);
-INSERT INTO `assignment` VALUES (4, NULL, '加班', '加班', '2023-04-24 05:12:09', 0, 0);
+INSERT INTO `assignment` VALUES (4, NULL, '加班', '加班', '2023-04-24 05:12:09', 1, 0);
 
 -- ----------------------------
 -- Table structure for clock_in
@@ -89,6 +71,26 @@ INSERT INTO `clock_in` VALUES (16, 1, 1, '2023-04-21 15:36:24', '2023-04-21 00:0
 INSERT INTO `clock_in` VALUES (17, 1, 2, '2023-04-21 15:36:25', '2023-04-21 00:00:00', 0);
 
 -- ----------------------------
+-- Table structure for contacts
+-- ----------------------------
+DROP TABLE IF EXISTS `contacts`;
+CREATE TABLE `contacts`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NULL DEFAULT NULL COMMENT '用户id',
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '联系人姓名',
+  `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '联系人电话',
+  `remarks` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '备注',
+  `is_deleted` tinyint NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `user_id`(`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '通讯录表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of contacts
+-- ----------------------------
+INSERT INTO `contacts` VALUES (1, NULL, '欧家俊', '15915095796', '1', 0);
+
+-- ----------------------------
 -- Table structure for customer
 -- ----------------------------
 DROP TABLE IF EXISTS `customer`;
@@ -116,7 +118,7 @@ CREATE TABLE `expense`  (
   `reason` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '报销原因',
   `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '报销描述',
   `create_time` datetime NULL DEFAULT NULL COMMENT '报销申请时间',
-  `status` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '申请状态',
+  `status` tinyint NULL DEFAULT NULL COMMENT '申请状态 2 未通过，1 通过，0 未审核',
   `user_id` int NULL DEFAULT NULL COMMENT '申请人id',
   `is_deleted` tinyint NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE,
@@ -138,7 +140,7 @@ CREATE TABLE `leave`  (
   `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '请假描述',
   `begin_time` datetime NULL DEFAULT NULL COMMENT '请假开始时间',
   `end_time` datetime NULL DEFAULT NULL COMMENT '请假结束时间',
-  `status` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '请假状态',
+  `status` tinyint NULL DEFAULT NULL COMMENT '状态 2 未通过，1 通过，0 未审核',
   `user_id` int NULL DEFAULT NULL COMMENT '请假人',
   `is_deleted` tinyint NULL DEFAULT 0 COMMENT '逻辑删除 1（true）已删除， 0（false）未删除',
   PRIMARY KEY (`id`) USING BTREE,
@@ -149,7 +151,6 @@ CREATE TABLE `leave`  (
 -- ----------------------------
 -- Records of leave
 -- ----------------------------
-INSERT INTO `leave` VALUES (13, '事假', '购物', '2020-11-08 16:00:00', '2020-11-08 16:00:00', '同意', 3, 0);
 
 -- ----------------------------
 -- Table structure for meeting_notice
@@ -236,11 +237,12 @@ INSERT INTO `notice` VALUES (11, '放假通知', '明天全体放假！', '2022-
 DROP TABLE IF EXISTS `project`;
 CREATE TABLE `project`  (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '项目名称',
+  `title` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '项目名称',
   `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '项目描述',
   `rate` int NULL DEFAULT 0 COMMENT '项目进度',
   `user_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '项目负责人',
   `create_time` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '项目创建时间',
+  `status` tinyint NULL DEFAULT NULL COMMENT '状态 2 未通过，1 通过，0 未审核',
   `is_deleted` tinyint NULL DEFAULT 0 COMMENT '逻辑删除 1（true）已删除， 0（false）未删除',
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `user_id`(`user_name`) USING BTREE
@@ -249,9 +251,11 @@ CREATE TABLE `project`  (
 -- ----------------------------
 -- Records of project
 -- ----------------------------
-INSERT INTO `project` VALUES (5, '项目1', '1', 12, 'ojj', NULL, 0);
-INSERT INTO `project` VALUES (6, '项目2', '2', 22, 'ojj', NULL, 0);
-INSERT INTO `project` VALUES (7, '项目3', '3', 33, 'ojj', NULL, 0);
+INSERT INTO `project` VALUES (5, '项目1', '1', 12, 'ojj', NULL, 1, 0);
+INSERT INTO `project` VALUES (6, '项目2', '2', 22, 'ojj', NULL, 1, 0);
+INSERT INTO `project` VALUES (7, '项目3', '3', 33, 'ojj', NULL, 1, 0);
+INSERT INTO `project` VALUES (9, '项目4', '4', 0, '欧家俊', '2023-04-25 11:57:19', 0, 0);
+INSERT INTO `project` VALUES (10, '5', '5', 0, '5', '2023-04-26 12:00:05', 0, 0);
 
 -- ----------------------------
 -- Table structure for quit
@@ -262,7 +266,7 @@ CREATE TABLE `quit`  (
   `reason` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '离职原因',
   `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '离职描述',
   `create_time` datetime NULL DEFAULT NULL COMMENT '离职申请时间',
-  `status` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '申请状态',
+  `status` tinyint NULL DEFAULT NULL COMMENT '申请状态 状态 2 未通过，1 通过，0 未审核',
   `user_id` int NULL DEFAULT NULL COMMENT '离职申请人',
   `is_deleted` tinyint NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE,
@@ -318,5 +322,6 @@ CREATE TABLE `user`  (
 INSERT INTO `user` VALUES (1, 'admin', 'admin', '1', NULL, NULL, '经理', 1, NULL, 0);
 INSERT INTO `user` VALUES (3, 'tony', 'tony', '3', '111', '111', '员工', 1, NULL, 0);
 INSERT INTO `user` VALUES (8, '欧家俊', 'ojj', '123456', '15915095796', '294557741@qq.com', '开发人员', 1, '2023-04-24 09:11:06', 0);
+INSERT INTO `user` VALUES (9, '莫子晴', 'mzq', '123456', '111', '111', '董事长', 2, '2023-04-25 11:49:41', 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
