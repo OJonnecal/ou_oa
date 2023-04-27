adccd82cc097a678a26b25ddda04ea80b09eb156
oujiajun <oujiajun@cnliqi.com>
2023-04-27 10:42:34 +0800

    添加离职申请审批接口

--- src/main/java/com/jjou/ouOffice/controller/QuitController.java
+++ src/main/java/com/jjou/ouOffice/controller/QuitController.java
@@ -1,9 +1,17 @@
 package com.jjou.ouOffice.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.jjou.ouOffice.common.Result;
+import com.jjou.ouOffice.entity.Quit;
+import com.jjou.ouOffice.service.QuitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 离职申请/审批控制器
@@ -14,6 +22,63 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequestMapping("/quit")
 public class QuitController {
+
+    @Autowired
+    private QuitService quitService;
+
+    @PostMapping(value = "/getApplyQuitList")
+    @ResponseBody
+    public Result getApplyQuitList() {
+        QueryWrapper<Quit> wrapper = new QueryWrapper<>();
+        wrapper.eq("status", 0).orderByDesc("create_time");
+        return Result.ok().data("applyQuitList", quitService.list(wrapper));
+    }
+
+    @PostMapping(value = "/getQuitList")
+    @ResponseBody
+    public Result getQuitList() {
+        QueryWrapper<Quit> wrapper = new QueryWrapper<>();
+        wrapper.eq("status", 1).orderByDesc("approve_time");
+        return Result.ok().data("quitList", quitService.list(wrapper));
+    }
+
+    @PostMapping(value = "/getFailQuitList")
+    @ResponseBody
+    public Result getFailQuitList() {
+        QueryWrapper<Quit> wrapper = new QueryWrapper<>();
+        wrapper.eq("status", 2).orderByDesc("approve_time");
+        return Result.ok().data("failQuitList", quitService.list(wrapper));
+    }
+
+    @PostMapping("/delQuit")
+    @ResponseBody
+    public Result delQuit(@RequestBody Quit quit){
+        boolean isSuccess = quitService.removeById(quit.getId());
+
+        if (isSuccess){
+            return Result.ok().message("删除请假申请成功！");
+        }else{
+            return Result.error().message("删除请假申请失败！");
+        }
+    }
+
+
+    @PostMapping("/addQuit")
+    @ResponseBody
+    public Result addQuit(@RequestBody Quit quit){
+        return quitService.addQuit(quit);
+    }
+
+    /**
+     * 离职审批
+     * @param quit
+     * @return
+     */
+    @PostMapping("/agreeQuit")
+    @ResponseBody
+    public Result agreeQuit(@RequestBody Quit quit){
+        return quitService.agreeQuit(quit);
+    }
 
 }
 
--- src/main/java/com/jjou/ouOffice/service/impl/QuitServiceImpl.java
+++ src/main/java/com/jjou/ouOffice/service/impl/QuitServiceImpl.java
@@ -1,10 +1,15 @@
 package com.jjou.ouOffice.service.impl;
 
+import com.jjou.ouOffice.common.Result;
 import com.jjou.ouOffice.entity.Quit;
 import com.jjou.ouOffice.mapper.QuitMapper;
 import com.jjou.ouOffice.service.QuitService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * <p>
@@ -17,4 +22,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class QuitServiceImpl extends ServiceImpl<QuitMapper, Quit> implements QuitService {
 
+    @Override
+    public Result addQuit(Quit quit) {
+        if(quit.getReason() == null || StringUtils.isEmpty(quit.getReason())){
+            return Result.error().message("离职原因不能为空！");
+        }
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        quit.setCreateTime(sdf.format(date));
+        save(quit);
+        return Result.ok().message("离职申请添加成功！");
+    }
+
+    @Override
+    public Result agreeQuit(Quit quit) {
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        quit.setApproveTime((sdf.format(date)));
+        boolean isSuccess = updateById(quit);
+
+        if (isSuccess) {
+            if(quit.getStatus() == 1) {
+                return Result.ok().message("离职申请通过！");
+            }else{
+                return Result.ok().message("离职申请拒绝！");
+            }
+        }else{
+            return Result.error().message("审批失败！");
+        }
+    }
 }
