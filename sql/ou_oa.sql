e3a73ad35ba897ed4f7506b1a358a52d2de81251
OuJiajun <95907237+OJonnecal@users.noreply.github.com>
2023-05-02 22:10:00 +0800

    修改离职时间字段类型

--- sql/ou_oa.sql
+++ sql/ou_oa.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80028
  File Encoding         : 65001
 
- Date: 01/05/2023 12:18:38
+ Date: 02/05/2023 22:07:00
 */
 
 SET NAMES utf8mb4;
@@ -109,7 +109,7 @@ CREATE TABLE `customer`  (
 -- ----------------------------
 -- Records of customer
 -- ----------------------------
-INSERT INTO `customer` VALUES (1, '欧家俊', '15915095796', 0, '2023-04-21 17:12:54', '111');
+INSERT INTO `customer` VALUES (1, '欧家俊', '15915095796', 0, '2023-04-21 17:12:54', '');
 INSERT INTO `customer` VALUES (6, '1', '1', 0, '2023-04-29 11:04:59', '1');
 
 -- ----------------------------
@@ -136,6 +136,7 @@ CREATE TABLE `expense`  (
 -- ----------------------------
 INSERT INTO `expense` VALUES (1, '1', '1', 1, '2023-04-27 10:54:10', '2023-04-27 10:55:06', 1, 8, 0);
 INSERT INTO `expense` VALUES (2, '2', '2', 2, '2023-04-27 10:55:11', '2023-04-27 10:55:12', 2, 8, 0);
+INSERT INTO `expense` VALUES (3, '1', '', NULL, '2023-05-02 10:23:30', NULL, 0, 8, 0);
 
 -- ----------------------------
 -- Table structure for leave
@@ -155,7 +156,7 @@ CREATE TABLE `leave`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `user_id`(`user_id`) USING BTREE,
   CONSTRAINT `leave_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 29 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '请假申请表' ROW_FORMAT = COMPACT;
+) ENGINE = InnoDB AUTO_INCREMENT = 30 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '请假申请表' ROW_FORMAT = COMPACT;
 
 -- ----------------------------
 -- Records of leave
@@ -169,6 +170,7 @@ INSERT INTO `leave` VALUES (25, '1', '1', NULL, NULL, 1, '2023-04-27 11:04:42', 
 INSERT INTO `leave` VALUES (26, '1', '1', '2023-04-05 16:00:00', '2023-04-06 16:00:00', 2, '2023-04-27 17:01:12', '2023-04-29 11:36:59', 8, 0);
 INSERT INTO `leave` VALUES (27, '9', '9', '2023-04-03 16:00:00', '2023-04-04 16:00:00', 0, '2023-04-27 17:03:28', NULL, 9, 0);
 INSERT INTO `leave` VALUES (28, '1', '1', '2023-04-04 16:00:00', '2023-04-05 16:00:00', 1, '2023-04-29 11:29:55', '2023-04-29 11:36:54', 8, 0);
+INSERT INTO `leave` VALUES (29, '1', '1', '2023-05-02 16:00:00', '2023-05-04 16:00:00', 0, '2023-05-02 10:22:29', NULL, 8, 0);
 
 -- ----------------------------
 -- Table structure for meeting_room
@@ -201,7 +203,7 @@ CREATE TABLE `notice`  (
   `is_show` tinyint NULL DEFAULT 1 COMMENT '是否展示，0不展示，1展示',
   `is_deleted` tinyint NULL DEFAULT 0 COMMENT '逻辑删除 1（true）已删除， 0（false）未删除',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 55 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '公告表' ROW_FORMAT = COMPACT;
+) ENGINE = InnoDB AUTO_INCREMENT = 58 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '公告表' ROW_FORMAT = COMPACT;
 
 -- ----------------------------
 -- Records of notice
@@ -250,7 +252,7 @@ CREATE TABLE `quit`  (
   `id` int NOT NULL AUTO_INCREMENT,
   `reason` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '离职原因',
   `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '离职描述',
-  `quit_time` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '离职时间',
+  `quit_time` datetime NULL DEFAULT NULL COMMENT '离职时间',
   `create_time` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '离职申请时间',
   `approve_time` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '离职审批时间',
   `status` tinyint NULL DEFAULT NULL COMMENT '申请状态 状态 2 未通过，1 通过，0 未审核',
@@ -259,14 +261,15 @@ CREATE TABLE `quit`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `user_id`(`user_id`) USING BTREE,
   CONSTRAINT `quit_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '离职申请表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '离职申请表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of quit
 -- ----------------------------
-INSERT INTO `quit` VALUES (1, '1', '1', '2023-04-28', '2023-04-27 17:20:01', NULL, 0, 8, 0);
-INSERT INTO `quit` VALUES (2, '2', '2', '2023/4/29', '2023-04-27 17:23:37', NULL, 0, 9, 0);
-INSERT INTO `quit` VALUES (3, '2', '2', '2023/4/30', '2023-04-27 17:23:47', NULL, 0, 9, 0);
+INSERT INTO `quit` VALUES (1, '1', '1', '2023-04-28 00:00:00', '2023-04-27 17:20:01', NULL, 0, 8, 0);
+INSERT INTO `quit` VALUES (2, '2', '2', '2023-04-29 00:00:00', '2023-04-27 17:23:37', NULL, 0, 9, 0);
+INSERT INTO `quit` VALUES (3, '2', '2', '2023-04-30 00:00:00', '2023-04-27 17:23:47', NULL, 0, 9, 0);
+INSERT INTO `quit` VALUES (4, '1', '1', '2023-05-30 00:00:00', '2023-05-02 10:24:25', NULL, 0, 8, 0);
 
 -- ----------------------------
 -- Table structure for top_contacts
@@ -280,13 +283,14 @@ CREATE TABLE `top_contacts`  (
   `is_deleted` tinyint NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `user_id`(`name`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '常用联系人表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '常用联系人表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of top_contacts
 -- ----------------------------
 INSERT INTO `top_contacts` VALUES (1, '欧家俊', '15915095796', '294557741@qq.com', 0);
 INSERT INTO `top_contacts` VALUES (6, '莫子晴', '19854571208', '111', 0);
+INSERT INTO `top_contacts` VALUES (11, '1', '1', '', 1);
 
 -- ----------------------------
 -- Table structure for user
@@ -299,19 +303,20 @@ CREATE TABLE `user`  (
   `pwd` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
   `phone` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号',
   `email` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
-  `position` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '职位',
+  `position` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '职位',
   `permission` int NULL DEFAULT NULL COMMENT '权限 1管理员',
   `create_time` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户创建时间',
   `is_deleted` tinyint UNSIGNED NULL DEFAULT 0 COMMENT '逻辑删除 1（true）已删除， 0（false）未删除',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户表' ROW_FORMAT = COMPACT;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户表' ROW_FORMAT = COMPACT;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'admin', 'admin', '1', NULL, NULL, '经理', 1, NULL, 0);
-INSERT INTO `user` VALUES (3, 'tony', 'tony', '3', '111', '111', '员工', 2, NULL, 0);
+INSERT INTO `user` VALUES (1, 'admin', 'admin', '1', NULL, NULL, '经理', 1, NULL, 1);
+INSERT INTO `user` VALUES (3, 'tony', 'tony', '3', '111', '111', '员工', 2, NULL, 1);
 INSERT INTO `user` VALUES (8, '欧家俊', 'ojj', '123456', '15915095796', '294557741@qq.com', '开发人员', 1, '2023-04-24 09:11:06', 0);
 INSERT INTO `user` VALUES (9, '莫子晴', 'mzq', '123456', '111', '111', '董事长', 2, '2023-04-25 11:49:41', 0);
+INSERT INTO `user` VALUES (10, '管理员', 'admin', 'admin', '1', '1', '管理员', 1, '2023-05-02 09:47:40', 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
